96a903a062227b7413f276fe9c918ec3fc60c1a1
Ivan Dolgikh <ivan.dolgikh1199@gmail.com>
2023-09-09 16:08:19 +0300

    add: App into arrow func

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+/*function App() {
   return (
     <div className="App">
      Let's go!
+    </div>
+  );
+}*/
+
+
+const App = () => {
+  return (
+    <div className="App">
+        simple html
+        <ul>
+            <li>html</li>
+            <li>css</li>
+            <li>js</li>
+            <li>react</li>
+        </ul>
     </div>
   );
 }
