ce3a58b70d42eeadc0f848479c769c81633e4040
Ivan Dolgikh <ivan.dolgikh1199@gmail.com>
2024-04-10 11:49:19 +0300

    add: lesson 95

--- src/index.tsx
+++ src/index.tsx
@@ -2,19 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import SamuraiJSApp from './App';
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 import {store} from "./redux/redux-store";
 import {Provider} from "react-redux";
 
 
 const rerenderEntireTree = () => {
     ReactDOM.render(
-            <BrowserRouter>
+            <HashRouter>
                 <Provider store={store}>
                     {/*<App />*/}
                     <SamuraiJSApp />
                 </Provider>
-            </BrowserRouter>,
+            </HashRouter>,
         document.getElementById('root')
     );
 }
