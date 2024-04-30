86c724e835bbf2bfc619a01c75926083d851d0de
Ivan Dolgikh <ivan.dolgikh1199@gmail.com>
2024-04-30 14:49:47 +0300

    fix: hashrouter

--- src/index.tsx
+++ src/index.tsx
@@ -2,19 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import SamuraiJSApp from './App';
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, HashRouter} from "react-router-dom";
 import {store} from "./redux/redux-store";
 import {Provider} from "react-redux";
 
 //todo: change surround to <HashRouter>
 const rerenderEntireTree = () => {
     ReactDOM.render(
-            <BrowserRouter>
+            // <BrowserRouter>
+            <HashRouter>
                 <Provider store={store}>
                     {/*<App />*/}
                     <SamuraiJSApp />
                 </Provider>
-            </BrowserRouter>,
+            {/*</BrowserRouter>,*/}
+            </HashRouter>,
         document.getElementById('root')
     );
 }
